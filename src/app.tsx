bd4a21887a5c27709c736dd2ba9ccae607878217
Jhon Peter Pacinos <jp.pacinos@gmail.com>
2025-05-24 03:24:34 +0000

    Apply eslint check-file rules

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
+import PWABadge from './pwa-badge.tsx'
 import './App.css'
 
 function App() {
