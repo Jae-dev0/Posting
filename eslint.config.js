7474ece6983c17ed44adb527b820aecc7fbd3071
Jhon Peter Pacinos <jp.pacinos@gmail.com>
2025-05-24 03:05:58 +0000

    Add rule to '@typescript-eslint/no-misused-promises' for handling void return

--- eslint.config.js
+++ eslint.config.js
@@ -36,6 +36,14 @@ export default tseslint.config(
       ],
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      '@typescript-eslint/no-misused-promises': [
+        2,
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
     },
   },
   eslintPluginPrettierRecommended, // Add the Prettier recommended configuration as the LAST item.
