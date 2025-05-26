771456039c509e042e96aeb06586a411a5129b04
Jhon Peter Pacinos <jp.pacinos@gmail.com>
2025-05-26 02:03:20 +0000

    Add ESLint rule to restrict cross-feature imports and enforce unidirectional codebase structure

--- eslint.config.js
+++ eslint.config.js
@@ -84,6 +84,39 @@ export default tseslint.config(
           alphabetize: { order: 'asc', caseInsensitive: true },
         },
       ],
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            // disables cross-feature imports:
+            // eg. src/features/feature-a should not import from src/features/feature-b, etc.
+            // {
+            //   target: './src/features/auth',
+            //   from: './src/features',
+            //   except: ['./auth'],
+            // },
+
+            // enforce unidirectional codebase:
+            // e.g. src/app can import from src/features but not the other way around
+            {
+              target: './src/features',
+              from: './src/app',
+            },
+
+            // e.g src/features and src/app can import from these shared modules but not the other way around
+            {
+              target: [
+                './src/components',
+                './src/hooks',
+                './src/lib',
+                './src/types',
+                './src/utils',
+              ],
+              from: ['./src/features', './src/app'],
+            },
+          ],
+        },
+      ],
     },
   },
   ...pluginQuery.configs['flat/recommended'],
