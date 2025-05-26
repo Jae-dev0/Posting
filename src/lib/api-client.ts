330d43b702e1179c735c2951f27d791dde0cab94
Jhon Peter Pacinos <jp.pacinos@gmail.com>
2025-05-26 00:51:16 +0000

    Add API client with Axios and request interceptor

---
+++ src/lib/api-client.ts
@@ -0,0 +1,19 @@
+import Axios, { InternalAxiosRequestConfig } from 'axios'
+
+import { env } from '@/config/env'
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = 'application/json'
+  }
+
+  // config.withCredentials = true
+
+  return config
+}
+
+export const api = Axios.create({
+  baseURL: env.API_URL,
+})
+
+api.interceptors.request.use(authRequestInterceptor)
