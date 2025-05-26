a2c7f99c88389b318eb955aa1bd1f1e8ce371a9a
Jhon Peter Pacinos <jp.pacinos@gmail.com>
2025-05-26 01:48:46 +0000

    Include HelmetProvider to AppProvider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode } from 'react'
+import { HelmetProvider } from 'react-helmet-async'
 
 import { PWABadge } from '@/components/ui/pwa-badge'
 import { queryConfig } from '@/lib/react-query'
@@ -15,13 +16,13 @@ export interface AppProviderProps {
 
 export function AppProvider({ children }: AppProviderProps) {
   return (
-    <>
+    <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         {children}
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
 
       <PWABadge />
-    </>
+    </HelmetProvider>
   )
 }
